3d247d4bfdb09fdbf28f6709835833cc3b1758ba
ShivaniMhatre <shivanikatkar97@gmail.com>
2024-01-03 12:58:47 +0530

    updates

--- myntra_frontend/src/Component/Config/API.js
+++ myntra_frontend/src/Component/Config/API.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 // const token = JSON.parse(localStorage.getItem("token"))
 
 const api = axios.create({
-    baseURL: 'http://localhost:4001',
+    // baseURL: 'http://localhost:4001',
+    baseURL:'https://myntra-3s3b.onrender.com'
     // headers:{'Authorization':`Bearer ${token}`}
 })
 
